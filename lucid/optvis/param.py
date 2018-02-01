dea02e457ab4058a89a5e33685cde492a1dfff6c
Christopher Olah <colah@google.com>
2018-01-31 20:50:41 -0800

    Restructure optvis.param into multiple submodueles

--- lucid/optvis/param.py
+++ lucid/optvis/param.py
@@ -176,26 +176,6 @@ def lowres_tensor(shape, underlying_shape, offset=None, sd=None):
   return t
 
 
-def _linear_decorelate_color(t):
-  t_flat = tf.reshape(t, [-1, 3])
-  color_correlation_normalized = color_correlation_svd_sqrt / max_norm_svd_sqrt
-  t_flat = tf.matmul(t_flat, color_correlation_normalized.T)
-  t = tf.reshape(t_flat, tf.shape(t))
-  return t
-
-
-def svd_color_clipped(t):
-  mean = [0.48, 0.46, 0.41]
-  t = mean + _linear_decorelate_color(t)
-  t = tf.clip_by_value(t, 0, 1)
-  return t
-
-
-def rgb_sigmoid(t, decorrelate=False):
-  t = t[..., :3]
-  if decorrelate:
-    t = _linear_decorelate_color(t)
-  return tf.nn.sigmoid(t)
 
 
 def fancy_colors(t):
@@ -216,67 +196,6 @@ def fancy_colors(t):
   return (1-S)*L + S*H_image
 
 
-def multi_interpolation_basis(n_objectives=6, n_interp_steps=5, width=128,
-                              channels=3):
-  """A paramaterization for interpolating between each pair of N objectives.
-
-  Sometimes you want to interpolate between optimizing a bunch of objectives,
-  in a paramaterization that encourages images to align.
-
-  Args:
-    n_objectives: number of objectives you want interpolate between
-    n_interp_steps: number of interpolation steps
-    width: width of intepolated images
-    channel
-
-  Returns:
-    A [n_objectives, n_objectives, n_interp_steps, width, width, channel]
-    shaped tensor, t, where the final [width, width, channel] should be
-    seen as images, such that the following properties hold:
-
-     t[a, b]    = t[b, a, ::-1]
-     t[a, i, 0] = t[a, j, 0] for all i, j
-     t[a, a, i] = t[a, a, j] for all i, j
-     t[a, b, i] = t[b, a, -i] for all i
-
-  """
-  N, M, W, Ch = n_objectives, n_interp_steps, width, channels
-
-  const_term = sum([lowres_tensor([W, W, Ch], [W/k, W/k, Ch])
-                    for k in [1, 2, 4, 8]])
-  const_term = tf.reshape(const_term, [1, 1, 1, W, W, Ch])
-
-  example_interps = [
-      sum([lowres_tensor([M, W, W, Ch], [2, W/k, W/k, Ch])
-           for k in [1, 2, 4, 8]])
-      for _ in range(N)]
-
-  example_basis = []
-  for n in range(N):
-    col = []
-    for m in range(N):
-      interp = example_interps[n] + example_interps[m][::-1]
-      col.append(interp)
-    example_basis.append(col)
-
-  interp_basis = []
-  for n in range(N):
-    col = [interp_basis[m][N-n][::-1] for m in range(n)]
-    col.append(tf.zeros([M, W, W, 3]))
-    for m in range(n+1, N):
-      interp = sum([lowres_tensor([M, W, W, Ch], [M, W/k, W/k, Ch])
-                    for k in [1, 2]])
-      col.append(interp)
-    interp_basis.append(col)
-
-  basis = []
-  for n in range(N):
-    col_ex = tf.stack(example_basis[n])
-    col_in = tf.stack(interp_basis[n])
-    basis.append(col_ex + col_in)
-  basis = tf.stack(basis)
-
-  return basis + const_term
 
 
 
