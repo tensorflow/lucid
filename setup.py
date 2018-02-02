f4b30a741b66b5628e57124b5ce9ee1a8eab23bc
Ludwig Schubert <ludwigschubert@google.com>
2018-02-01 16:13:27 -0800

    Minor setup changes

--- setup.py
+++ setup.py
@@ -18,6 +18,8 @@ from __future__ import absolute_import, division, print_function
 
 from setuptools import setup, find_packages
 
+version = '0.0.2'
+
 test_deps = [
   'future',
   'twine',
@@ -31,13 +33,14 @@ extras = {
 setup(
   name = 'lucid',
   packages = find_packages(exclude=[]),
-  version = '0.0.1',
+  version = version,
   description = ('Collection of infrastructure and tools for research in '
     'neural network interpretability.'),
   author = 'The Deepviz Authors',
   author_email = 'deepviz@google.com',
   url = 'https://github.com/tensorflow/lucid',
-  download_url = 'https://github.com/tensorflow/lucid/archive/0.0.1.tar.gz',
+  download_url = ('https://github.com/tensorflow/lucid'
+    '/archive/v{}.tar.gz'.format(version)),
   license = 'Apache License 2.0',
   keywords = ['tensorflow', 'tensor', 'machine learning', 'neural networks',
     'convolutional neural networks', 'feature visualization', 'optimization'],
@@ -49,7 +52,7 @@ setup(
     'pillow',
   ],
   tests_require = test_deps,
-  extras_require=extras,
+  extras_require = extras,
   classifiers = [
     'Development Status :: 2 - Pre-Alpha',
     'Intended Audience :: Science/Research',
@@ -60,10 +63,7 @@ setup(
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    # 'Programming Language :: Python :: 3.4',
-    # 'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
-    # 'Programming Language :: Python :: 3.7',
     'Topic :: Scientific/Engineering',
     'Topic :: Scientific/Engineering :: Artificial Intelligence',
     'Topic :: Scientific/Engineering :: Mathematics',
