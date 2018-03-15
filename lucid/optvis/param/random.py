c50ab39efe038ebb923187b4354aec64eba4abbf
Christopher Olah <colah@google.com>
2018-03-15 15:58:58 -0700

    Fix imports for param.random

--- lucid/optvis/param/random.py
+++ lucid/optvis/param/random.py
@@ -1,9 +1,27 @@
-import lucid.optvis.param.spatial as spatial
-import lucid.optvis.param.color as color
+# Copyright 2018 The Lucid Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import tensorflow as tf
+import numpy as np
+
+from lucid.optvis.param.color import to_valid_rgb
+
 
 def image_sample(shape, decorrelate=True, sd=None, decay_power=1):
   raw_spatial = rand_fft_image(shape, sd=sd, decay_power=decay_power)
-  return color.to_valid_rgb(raw_spatial, decorrelate=decorrelate)
+  return to_valid_rgb(raw_spatial, decorrelate=decorrelate)
 
 def rand_fft_image(shape, sd=None, decay_power=1):
   b, h, w, ch = shape
@@ -11,7 +29,7 @@ def rand_fft_image(shape, sd=None, decay_power=1):
   
   imgs = []
   for _ in range(b):
-    freqs = spatial._rfft2d_freqs(h, w)
+    freqs = _rfft2d_freqs(h, w)
     fh, fw = freqs.shape
     spectrum_var = sd*tf.random_normal([2, ch, fh, fw], dtype="float32")
     spectrum = tf.complex(spectrum_var[0], spectrum_var[1])
@@ -28,3 +46,14 @@ def rand_fft_image(shape, sd=None, decay_power=1):
     img = tf.transpose(img, [1, 2, 0])
     imgs.append(img)
   return tf.stack(imgs)/4.
+
+def _rfft2d_freqs(h, w):
+  """Compute 2d spectrum frequences."""
+  fy = np.fft.fftfreq(h)[:, None]
+  # when we have an odd input dimension we need to keep one additional
+  # frequency and later cut off 1 pixel
+  if w % 2 == 1:
+    fx = np.fft.fftfreq(w)[:w//2+2]
+  else:
+    fx = np.fft.fftfreq(w)[:w//2+1]
+  return np.sqrt(fx*fx + fy*fy)
