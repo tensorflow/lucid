72821ac85205dc6ebc5dc153c83f40fce98d38c8
Ludwig Schubert <github-email@ludwigschubert.de>
2018-04-16 20:44:54 -0700

    Increment version to 0.0.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from __future__ import absolute_import, division, print_function
 
 from setuptools import setup, find_packages
 
-version = '0.0.6'
+version = '0.0.7'
 
 test_deps = [
   'future',
