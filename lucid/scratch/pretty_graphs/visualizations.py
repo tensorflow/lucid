049b696baa973290b2837c9b5e8fb62692f9546f
Ludwig Schubert <schubert@openai.com>
2018-10-24 13:23:25 -0700

    update pretty_graphs to Python 3 style print statements

--- lucid/scratch/pretty_graphs/visualizations.py
+++ lucid/scratch/pretty_graphs/visualizations.py
@@ -11,7 +11,7 @@ from lucid.misc.io import show, load
 def display_model(model, tooltips=None):
   tooltips = tooltips or {};
   render = complete_render_model_graph(model)
-    
+
   svg = """
     <script src="https://d3js.org/d3.v5.min.js"></script>
     <style>
@@ -32,7 +32,7 @@ def display_model(model, tooltips=None):
         transition: opacity 0.2s, left 0.2s, top 0.2s;
       }
     </style>
-    
+
     <div style="width: %spx; height: %spx; position: relative" id="container">
     <svg width="100%%" height="100%%" style="position: absolute; top: 0px; left:0px;">
 
@@ -52,30 +52,30 @@ def display_tooltips_name(node_selector, tooltip_key_attr, container_selector="#
     var container = d3.select("{container_selector}");
     var tooltip_div = container.append("div");
     tooltip_div.classed("tooltip", true).style("opacity", "0.0");
-    
+
     function show_node() {{
       var node = d3.select(this);
       var svg_box = container.node().getBoundingClientRect();
       var box = node.node().getBoundingClientRect();
       var name = node.attr("{tooltip_key_attr}");
-      
 
-      tooltip_div.html(name);   
+
+      tooltip_div.html(name);
       tooltip_div.style("opacity", "1.0")
         .style("left", (box.x + 4 - svg_box.x)+"px")
         .style("top", (box.y + box.height + 12 - svg_box.y)+"px");
 
     }}
-    
+
     d3.selectAll("{node_selector}")
       .on("mouseover", show_node)
       .on("mouseout", () => tooltip_div.style("opacity", "0.0") );
     </script>
     """.format(**locals())
-  #print html
+  #print(html)
   _display_html(html)
-  
-  
+
+
 def display_tooltips(node_selector, tooltip_key_attr, tooltips, container_selector="#output-body"):
   tooltips_json = json.dumps(tooltips)
   html = """
@@ -84,9 +84,9 @@ def display_tooltips(node_selector, tooltip_key_attr, tooltips, container_select
     var container = d3.select("{container_selector}");
     var tooltip_div = container.append("div");
     tooltip_div.classed("tooltip", true).style("opacity", "0.0");
-    
+
     var tooltips = {tooltips_json};
-    
+
     function show_node() {{
       var node = d3.select(this);
       var svg_box = container.node().getBoundingClientRect();
@@ -94,19 +94,19 @@ def display_tooltips(node_selector, tooltip_key_attr, tooltips, container_select
       var name = node.attr("{tooltip_key_attr}");
 
       if (name in tooltips){{
-        tooltip_div.html(tooltips[name]);   
+        tooltip_div.html(tooltips[name]);
         tooltip_div.style("opacity", "1.0")
           .style("left", (box.x + 4 - svg_box.x)+"px")
           .style("top", (box.y + box.height + 12 - svg_box.y)+"px");
       }}
     }}
-    
+
     d3.selectAll("{node_selector}")
       .on("mouseover", show_node)
       .on("mouseout", () => tooltip_div.style("opacity", "0.0") );
     </script>
     """.format(**locals())
-  #print html
+  #print(html)
   _display_html(html)
 
 
@@ -124,14 +124,14 @@ def p(point, dy=0):
 
 def model_align_display(model1, model2, lines, middle_sep=150):
 
-  
+
   render1 = complete_render_model_graph(model1)
   render2 = complete_render_model_graph(model2)
   shape1, shape2 = render1["shape"], render2["shape"]
   inner1, inner2 = render1["svg_inner"], render2["svg_inner"]
   W = max(shape1[0], shape2[0]) + 20
   H = shape1[1] + middle_sep + shape2[1] + 20
-  
+
   paths = []
   for (name1, name2), weight in lines.items():
     box1, box2 = get_box(render1, name1), get_box(render2, name2)
@@ -142,7 +142,7 @@ def model_align_display(model1, model2, lines, middle_sep=150):
     info = "data-tf-src=\"%s\" data-tf-dest=\"%s\" class=\"comparison-edge\" " % (name1, name2)
     path = "<path d=\"%s\" style=\"%s\" %s ></path>" % (d, style, info)
     paths.append(path)
-  
+
   svg = """
     <script src="https://d3js.org/d3.v5.min.js"></script>
     <style>
@@ -157,7 +157,7 @@ def model_align_display(model1, model2, lines, middle_sep=150):
       }
       .comparison-edge {
         fill: none;
-        stroke-linecap: round; 
+        stroke-linecap: round;
         transition: opacity 0.2s;
       }
       .tooltip {
@@ -168,7 +168,7 @@ def model_align_display(model1, model2, lines, middle_sep=150):
         transition: opacity 0.2s, left 0.2s, top 0.2s;
       }
     </style>
-    
+
     <svg width=%s height=%s>
 
     <g transform="translate(0, 0)" id="compare-top" class="model">
@@ -181,7 +181,7 @@ def model_align_display(model1, model2, lines, middle_sep=150):
       %s
     </g>
     </svg>
-    
+
     <script>
     var edges = d3.selectAll(".comparison-edge");
 
@@ -195,7 +195,7 @@ def model_align_display(model1, model2, lines, middle_sep=150):
         pres.style("opacity", (src == name)? 1.0 : 0.0)
       })
     }}
-    
+
     function hide_edges_bottom() {{
       var node = d3.select(this);
       var name = node.attr("data-tf-name") || node.attr("data-group-tf-name");
@@ -205,11 +205,11 @@ def model_align_display(model1, model2, lines, middle_sep=150):
         pres.style("opacity", (src == name)? 1.0 : 0.0)
       })
     }}
-    
+
     d3.selectAll("#compare-top .node, #compare-top .background")
       .on("mouseover.edges", hide_edges_top)
       .on("mouseout.edges", () => edges.style("opacity", "1.0") );
-      
+
     d3.selectAll("#compare-bottom .node, #compare-bottom .background")
       .on("mouseover.edges", hide_edges_bottom)
       .on("mouseout.edges", () => edges.style("opacity", "1.0") );
@@ -217,7 +217,7 @@ def model_align_display(model1, model2, lines, middle_sep=150):
     """ % (W, H, inner1, "\n".join(paths), shape1[1] + middle_sep, inner2)
 
   _display_html(svg)
-  
-  
+
+
 def vpad(space):
   _display_html("""<div style="width:20px; height: %spx"></div>""" % space)
