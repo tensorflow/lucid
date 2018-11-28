a6e1cbcbf4bd031580288916b55481961ef84eca
Ludwig Schubert <schubert@openai.com>
2018-11-27 16:32:41 -0800

    Adds tests for spatial parameterizations

--- lucid/optvis/param/images.py
+++ lucid/optvis/param/images.py
@@ -1,4 +1,3 @@
-
 # Copyright 2018 The Lucid Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,21 +16,21 @@
 """High-level wrapper for paramaterizing images."""
 
 
-import numpy as np
 import tensorflow as tf
 
 from lucid.optvis.param.color import to_valid_rgb
-from lucid.optvis.param.spatial import naive, fft_image
+from lucid.optvis.param.spatial import pixel_image, fft_image
+
 
 def image(w, h=None, batch=None, sd=None, decorrelate=True, fft=True, alpha=False):
-  h = h or w
-  batch = batch or 1
-  channels = 4 if alpha else 3
-  shape = [batch, w, h, channels]
-  param_f = fft_image if fft else naive
-  t = param_f(shape, sd=sd)
-  rgb = to_valid_rgb(t[..., :3], decorrelate=decorrelate, sigmoid=True)
-  if alpha:
-    a = tf.nn.sigmoid(t[..., 3:])
-    return tf.concat([rgb, a], -1)
-  return rgb
+    h = h or w
+    batch = batch or 1
+    channels = 4 if alpha else 3
+    shape = [batch, w, h, channels]
+    param_f = fft_image if fft else pixel_image
+    t = param_f(shape, sd=sd)
+    rgb = to_valid_rgb(t[..., :3], decorrelate=decorrelate, sigmoid=True)
+    if alpha:
+        a = tf.nn.sigmoid(t[..., 3:])
+        return tf.concat([rgb, a], -1)
+    return rgb
--- lucid/optvis/param/random.py
+++ lucid/optvis/param/random.py
@@ -17,43 +17,35 @@ import tensorflow as tf
 import numpy as np
 
 from lucid.optvis.param.color import to_valid_rgb
+from lucid.optvis.param.spatial import rfft2d_freqs
 
 
 def image_sample(shape, decorrelate=True, sd=None, decay_power=1):
-  raw_spatial = rand_fft_image(shape, sd=sd, decay_power=decay_power)
-  return to_valid_rgb(raw_spatial, decorrelate=decorrelate)
+    raw_spatial = rand_fft_image(shape, sd=sd, decay_power=decay_power)
+    return to_valid_rgb(raw_spatial, decorrelate=decorrelate)
 
+
+# TODO: DRY with regard to fft_image from lucid.optvis.param.spatial
 def rand_fft_image(shape, sd=None, decay_power=1):
-  b, h, w, ch = shape
-  sd = 0.01 if sd is None else sd
-  
-  imgs = []
-  for _ in range(b):
-    freqs = _rfft2d_freqs(h, w)
-    fh, fw = freqs.shape
-    spectrum_var = sd*tf.random_normal([2, ch, fh, fw], dtype="float32")
-    spectrum = tf.complex(spectrum_var[0], spectrum_var[1])
-    spertum_scale = 1.0 / np.maximum(freqs, 1.0/max(h, w))**decay_power
-    # Scale the spectrum by the square-root of the number of pixels
-    # to get a unitary transformation. This allows to use similar
-    # leanring rates to pixel-wise optimisation.
-    spertum_scale *= np.sqrt(w*h)
-    scaled_spectrum = spectrum * spertum_scale
-    img = tf.spectral.irfft2d(scaled_spectrum)
-    # in case of odd input dimension we cut off the additional pixel
-    # we get from irfft2d length computation
-    img = img[:ch, :h, :w]
-    img = tf.transpose(img, [1, 2, 0])
-    imgs.append(img)
-  return tf.stack(imgs)/4.
+    b, h, w, ch = shape
+    sd = 0.01 if sd is None else sd
 
-def _rfft2d_freqs(h, w):
-  """Compute 2d spectrum frequences."""
-  fy = np.fft.fftfreq(h)[:, None]
-  # when we have an odd input dimension we need to keep one additional
-  # frequency and later cut off 1 pixel
-  if w % 2 == 1:
-    fx = np.fft.fftfreq(w)[:w//2+2]
-  else:
-    fx = np.fft.fftfreq(w)[:w//2+1]
-  return np.sqrt(fx*fx + fy*fy)
+    imgs = []
+    for _ in range(b):
+        freqs = rfft2d_freqs(h, w)
+        fh, fw = freqs.shape
+        spectrum_var = sd * tf.random_normal([2, ch, fh, fw], dtype="float32")
+        spectrum = tf.complex(spectrum_var[0], spectrum_var[1])
+        spertum_scale = 1.0 / np.maximum(freqs, 1.0 / max(h, w)) ** decay_power
+        # Scale the spectrum by the square-root of the number of pixels
+        # to get a unitary transformation. This allows to use similar
+        # leanring rates to pixel-wise optimisation.
+        spertum_scale *= np.sqrt(w * h)
+        scaled_spectrum = spectrum * spertum_scale
+        img = tf.spectral.irfft2d(scaled_spectrum)
+        # in case of odd input dimension we cut off the additional pixel
+        # we get from irfft2d length computation
+        img = img[:ch, :h, :w]
+        img = tf.transpose(img, [1, 2, 0])
+        imgs.append(img)
+    return tf.stack(imgs) / 4.0
