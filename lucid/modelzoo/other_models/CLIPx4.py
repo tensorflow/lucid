068f57fddb55b5b45bc776ee16ea1bb90535f7d2
Gabriel Goh <ggoh@openai.com>
2021-03-04 11:34:15 -0800

    added clip model

---
+++ lucid/modelzoo/other_models/CLIPx4.py
@@ -0,0 +1,23 @@
+# Copyright 2018 The Lucid Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from lucid.modelzoo.vision_base import Model
+
+class CLIPImage(Model):
+    image_value_range = (0, 255)
+    input_name = 'input_image'
+    model_name = "RN50_4x"
+    image_shape = [288, 288, 3]
+    model_path = "https://openaipublic.blob.core.windows.net/clip/tf/RN50_4x/084ee9c176da32014b0ebe42cd7ca66e/image32.pb"
