6458981bd57e71e2d97607949a2a65239bc19956
Ludwig Schubert <git@ludwigschubert.de>
2021-03-12 10:52:52 +0000

    Require Python 3.7 as numpy dependency does so

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Scientific/Engineering :: Mathematics",
