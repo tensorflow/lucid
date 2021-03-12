62bf069a67afc3acf70bfb30b99703c72248e715
Ludwig Schubert <git@ludwigschubert.de>
2021-03-12 10:51:54 +0000

    Correct import paths for Clip model

--- lucid/modelzoo/other_models/Clip.py
+++ lucid/modelzoo/other_models/Clip.py
@@ -15,9 +15,9 @@
 
 from lucid.modelzoo.vision_base import Model
 
-class CLIPImage(Model):
+class Clip_ResNet50_4x(Model):
     image_value_range = (0, 255)
     input_name = 'input_image'
-    model_name = "RN50_4x"
+    model_name = "Clip_ResNet50_4x"
     image_shape = [288, 288, 3]
-    model_path = "gs://modelzoo/vision/other_models/Clip_ResNet50.pb"
+    model_path = "gs://modelzoo/vision/other_models/Clip_ResNet50_4x.pb"
