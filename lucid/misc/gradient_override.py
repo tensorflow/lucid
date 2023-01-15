510f8e6251c6b92c7ec23d607df1f18f721f7332
Marco Carosi <ercaronte@gmail.com>
2023-01-15 23:31:02 +0100

    Fixing warning messages with TF 1.15.5, part 3

--- lucid/misc/gradient_override.py
+++ lucid/misc/gradient_override.py
@@ -154,7 +154,7 @@ def use_gradient(grad_f):
         state["out_value"] = out_value
 
       store_name = "store_" + f.__name__
-      store = tf.py_func(store_out, [out], (), stateful=True, name=store_name)
+      store = tf.compat.v1.py_func(store_out, [out], (), stateful=True, name=store_name)
 
       # Next, we create the mock function, with an overriden gradient.
       # Note that we need to make sure store gets evaluated before the mock
@@ -167,8 +167,7 @@ def use_gradient(grad_f):
       with tf.control_dependencies([store]):
         with gradient_override_map({"PyFunc": grad_f_name}):
           mock_name = "mock_" + f.__name__
-          mock_out = tf.py_func(mock_f, inputs, out.dtype, stateful=True,
-                                name=mock_name)
+          mock_out = tf.compat.v1.py_func(mock_f, inputs, out.dtype, stateful=True, name=mock_name)
           mock_out.set_shape(out.get_shape())
 
       # Finally, we can return the mock.
